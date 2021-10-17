f6d1d9ae03fc08899b3db4a2c08702885ea1d6e4
unknown <finn.beer@gmail.com>
2021-10-17 17:07:28 +0200

    fixed default value

--- backend/app/models/user_solution.py
+++ backend/app/models/user_solution.py
@@ -5,10 +5,10 @@ from sqlalchemy import JSON, Column, ForeignKey, Integer, Numeric, text
 class UserSolution(Base):
     user_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
     task_id = Column(Integer, ForeignKey('task.id'), primary_key=True)
-    percentage_solved = Column(Numeric(5, 2), server_default=text("0"))
+    percentage_solved = Column(Numeric(5, 2), server_default=text("0"), default=0)
     base_task_id = Column(Integer, ForeignKey('basetask.id'))
     task_group_id = Column(Integer, ForeignKey('taskgroup.id'))
     course_id = Column(Integer, ForeignKey('course.id'))
     solution_data = Column(JSON, nullable=False)
     task_result = Column(JSON, nullable=True)
-    failed_attempts = Column(Integer, nullable=False, server_default=text("0"))
+    failed_attempts = Column(Integer, nullable=False, server_default=text("0"), default=0)
