3155a094bd6f6f7375ef6378af6f7868f5d17c47
James Neumann <james@pop-os.localdomain>
2021-10-26 14:00:43 +0200

    Jest test setup and first tests

--- frontend/src/model/user.ts
+++ frontend/src/model/user.ts
@@ -2,8 +2,8 @@ export interface User {
   id: number;
   email: string;
   firstname: string;
-  middlename: string;
+  middlename?: string;
   lastname: string;
   is_active: boolean;
-  is_superuser: true;
+  is_superuser: boolean;
 }
---
+++ frontend/test/utils/app.state.test.ts
@@ -0,0 +1,23 @@
+import {appState} from '../../src/utils/app.state';
+import {User} from '../../src/model/user';
+
+describe('App State', () => {
+    it('Should contain user', () => {
+        expect(appState.user).toBeNull();
+    })
+
+    it('Should set user', () => {
+
+        const user: User = {
+            id: 1,
+            email: "Test@test.de",
+            firstname: "Test",
+            is_active: true,
+            is_superuser: true,
+            lastname: "Test",
+        };
+
+        appState.user = user;
+        expect(appState.user).toEqual(user);
+    })
+})
