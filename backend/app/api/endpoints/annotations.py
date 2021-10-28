04ff4bcc18c170b33cf15b08501a66c48e500da9
Jannes <jannes@neemann.net>
2021-10-28 21:26:31 +0200

    Added Temp directory of pycharm

---
+++ backend/app/api/endpoints/annotations.py
@@ -0,0 +1,18 @@
+from typing import Any
+
+from fastapi import APIRouter, Depends, UploadFile, File
+
+from app.api.deps import get_current_active_user
+from app.core.polygon_extractor import convert_image_to_annotations
+from app.models.user import User
+
+
+router = APIRouter()
+
+@router.post('/convert')
+async def convert_image(*, file: UploadFile = File(...),
+                        current_user: User = Depends(get_current_active_user)) -> Any:
+    contents = await file.read()
+    result = convert_image_to_annotations(contents)
+    return result
+
