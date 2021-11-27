18bc568b3544c7836805526b555394cf2c5d9f71
Jannes <jannes@neemann.net>
2021-11-27 21:20:22 +0100

    Add Sentry diagnostics

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,3 +1,5 @@
+import os
+
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 from starlette.middleware.gzip import GZipMiddleware
@@ -6,6 +8,15 @@ from starlette.staticfiles import StaticFiles
 from app.api.api import api_router
 from app.core.config import settings
 from app.utils.minio_client import MinioClient, minio_client
+
+
+if "SENTRY_METRICS" in os.environ:
+    if os.environ["SENTRY_METRICS"]:
+        import sentry_sdk
+        sentry_sdk.init(
+            "https://d639a3c655d24ddd8c82d877643460a7@o1079273.ingest.sentry.io/6083940",
+            traces_sample_rate=0.8
+        )
 
 app = FastAPI()
 
@@ -33,6 +44,7 @@ app.add_middleware(
 
 @app.get("/")
 def root():
+    division_by_zero = 1 / 0
     return {"Hello": "World"}
 
 
