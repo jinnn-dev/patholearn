ba4664bbfc079031913775a0e9baefac4f214f1c
James Neumann <jannesneemann@gmail.com>
2022-06-07 10:52:14 +0200

    Clean up entry script

--- backend/entry.sh
+++ backend/entry.sh
@@ -1,16 +1,12 @@
 #!/bin/sh
 
-echo "HOST: $MYSQL_HOST"
-echo "PASSWORD: $MYSQL_ROOT_PASSWORD"
-echo "DATABASE: $MYSQL_DATABASE"
-
-while ! mysql --host=$MYSQL_HOST --port=3306 -u root -p"$MYSQL_ROOT_PASSWORD" -e "show databases;" > /dev/null 2>&1; do
+while ! mysql --host="$MYSQL_HOST" --port=3306 -u root -p"$MYSQL_ROOT_PASSWORD" -e "show databases;" > /dev/null 2>&1; do
     echo "Waiting for MySQL..."
     sleep 1
 done
 
 echo "Initializing databases..."
-mysql --host=$MYSQL_HOST --port=3306 -u root -p"$MYSQL_ROOT_PASSWORD" -e "create database if not exists ${MYSQL_DATABASE};"
+mysql --host="$MYSQL_HOST" --port=3306 -u root -p"$MYSQL_ROOT_PASSWORD" -e "create database if not exists ${MYSQL_DATABASE};"
 
 echo "Running migrations..."
 alembic upgrade head
