58765772b7167712504d6af78ed739afe2d152f9
James Neumann <jannesneemann@gmail.com>
2022-09-28 14:44:55 +0200

    Remove visualizer plugin

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,7 @@
 import vue from '@vitejs/plugin-vue';
-import { visualizer } from 'rollup-plugin-visualizer';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    visualizer(),
-    vue()
-  ]
+  plugins: [vue()]
 });
