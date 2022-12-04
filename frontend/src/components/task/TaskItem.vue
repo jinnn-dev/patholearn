d605e46d033a401de55ae872e8237e9941e831aa
James Neumann <jannesneemann@gmail.com>
2022-12-04 10:57:50 +0100

    Fixes #57

--- frontend/src/components/task/TaskItem.vue
+++ frontend/src/components/task/TaskItem.vue
@@ -1,4 +1,4 @@
-<script lang='ts' setup>
+<script lang="ts" setup>
 import { PropType, ref } from 'vue';
 import { UserSolution } from '../../model/userSolution';
 import Icon from '../general/Icon.vue';
@@ -45,41 +45,41 @@ const downloadUserSolutions = () => {
 </script>
 <template>
   <div
-    :title='question'
-    class='transition flex flex-col items-center my-2 mx-2 p-2 bg-gray-500 rounded-xl hover:bg-gray-400'
+    :title="question"
+    class="transition flex flex-col items-center my-2 mx-2 p-2 bg-gray-500 rounded-xl hover:bg-gray-400"
   >
-    <div class='flex w-full'>
-      <div v-if='!isOwner'>
+    <div class="flex w-full">
+      <div v-if="!isOwner">
         <Icon
-          v-if='!userSolution || userSolution?.solution_data?.length === 0 || !userSolution.task_result'
-          class='text-gray-200'
-          name='circle'
+          v-if="!userSolution || !userSolution.solution_data || !userSolution?.task_result"
+          class="text-gray-200"
+          name="circle"
         ></Icon>
         <Icon
-          v-else-if='userSolution.task_result.task_status === TaskStatus.CORRECT'
-          class='text-green-400'
-          name='check-circle'
+          v-else-if="userSolution.task_result?.task_status === TaskStatus.CORRECT"
+          class="text-green-400"
+          name="check-circle"
         ></Icon>
-        <Icon v-else class='text-red-500' name='x-circle' />
+        <Icon v-else class="text-red-500" name="x-circle" />
       </div>
-      <div class='ml-2 w-full mx-2 break-all'>
+      <div class="ml-2 w-full mx-2 break-all">
         {{ question }}
       </div>
     </div>
-    <role-only v-if='isOwner'>
-      <div class='flex'>
-        <Icon v-if='showDownload' class='text-xl' name='download-simple' @click.stop='downloadUserSolutions' />
-        <Icon class='text-xl mx-2' name='pencil-simple' @click.stop='editTask' />
-        <Icon class='text-red-400 text-xl' name='trash' @click.stop='showDeleteTask = true' />
+    <role-only v-if="isOwner">
+      <div class="flex">
+        <Icon v-if="showDownload" class="text-xl" name="download-simple" @click.stop="downloadUserSolutions" />
+        <Icon class="text-xl mx-2" name="pencil-simple" @click.stop="editTask" />
+        <Icon class="text-red-400 text-xl" name="trash" @click.stop="showDeleteTask = true" />
       </div>
     </role-only>
 
     <confirm-dialog
-      :show='showDeleteTask'
-      detail='Alle Lösungen der Nutzer werden ebenfalls gelöscht'
-      header='Aufgabe löschen'
-      @confirmation='deleteTask'
-      @reject='showDeleteTask = false'
+      :show="showDeleteTask"
+      detail="Alle Lösungen der Nutzer werden ebenfalls gelöscht"
+      header="Aufgabe löschen"
+      @confirmation="deleteTask"
+      @reject="showDeleteTask = false"
     >
     </confirm-dialog>
   </div>
