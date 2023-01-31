1e021cee0648ccbcb7f0fbe1b4273b6b1457446d
James Neumann <jannesneemann@gmail.com>
2023-01-31 16:49:56 +0100

    Fix key

--- frontend/src/components/task/hint/Hint.vue
+++ frontend/src/components/task/hint/Hint.vue
@@ -1,4 +1,4 @@
-<script lang='ts' setup>
+<script lang="ts" setup>
 import { PropType } from 'vue';
 import { TaskHint } from '../../../model/task/taskHint';
 import { TaskService } from '../../../services/task.service';
@@ -22,28 +22,28 @@ async function deleteHint() {
 }
 </script>
 <template>
-  <div class='rounded-lg h-full bg-gray-500/40 flex w-full mb-2 flex-col p-2'>
-    <div class='flex justify-between items-center w-full mb-4'>
-      <div class='text-gray-200 font-bold'>
+  <div class="rounded-lg h-full bg-gray-500/40 flex w-full mb-2 flex-col p-2">
+    <div class="flex justify-between items-center w-full mb-4">
+      <div class="text-gray-200 font-bold">
         Wird nach
-        <span class='text-xl font-semibold text-highlight-500'>{{ hint.needed_mistakes }}</span>
+        <span class="text-xl font-semibold text-highlight-500">{{ hint.needed_mistakes }}</span>
         Fehlern angezeigt
       </div>
-      <div class='flex gap-2'>
-        <primary-button bgColor='bg-gray-500' class='w-8 h-8' type='button' @click="$emit('edit', hint)">
-          <Icon name='pencil' />
+      <div class="flex gap-2">
+        <primary-button bgColor="bg-gray-500" class="w-8 h-8" type="button" @click="$emit('edit', hint)">
+          <Icon name="pencil" />
         </primary-button>
-        <primary-button bgColor='bg-red-500' class='w-8 h-8' type='button'>
-          <Icon name='trash' @click='deleteHint' />
+        <primary-button bgColor="bg-red-500" class="w-8 h-8" type="button">
+          <Icon name="trash" @click="deleteHint" />
         </primary-button>
       </div>
     </div>
     <div>
       {{ hint.content }}
     </div>
-    <div class='my-2 flex gap-2'>
-      <div v-for='image in hint.images' :key='image.id' class='h-20 w-20 bg-gray-500 rounded-lg'>
-        <UploadPreviewImage :imgSrc="SLIDE_IMAGE_URL + '/' + image.image_name" :preview='true' />
+    <div class="my-2 flex gap-2">
+      <div v-for="image in hint.images" :key="image.image_name" class="h-20 w-20 bg-gray-500 rounded-lg">
+        <UploadPreviewImage :imgSrc="SLIDE_IMAGE_URL + '/' + image.image_name" :preview="true" />
       </div>
     </div>
   </div>
