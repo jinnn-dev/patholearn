3e1f182b4dfac5a96fb016180ff21a1a8eb2599f
James Neumann <jannesneemann@gmail.com>
2023-01-31 16:52:08 +0100

    Clean up ErrorBar

--- frontend/src/components/general/ErrorBar.vue
+++ frontend/src/components/general/ErrorBar.vue
@@ -1,4 +1,4 @@
-<script lang='ts' setup>
+<script lang="ts" setup>
 import { errorState } from '../../services/error-handler';
 import ErrorNotification from './ErrorNotification.vue';
 
@@ -7,29 +7,14 @@ const removeItem = (index: number) => {
 };
 </script>
 <template>
-  <div class='fixed z-[99] bottom-4 left-1/2 transform -translate-x-1/2'>
+  <div class="fixed z-[99] bottom-4 left-1/2 transform -translate-x-1/2">
     <error-notification
-      v-for='(error, index) in errorState'
-      :key='index'
-      :error='error'
-      :index='index'
-      @expired='removeItem($event)'
+      v-for="(error, index) in errorState"
+      :key="index"
+      :error="error"
+      :index="index"
+      @expired="removeItem($event)"
     >
     </error-notification>
-
-    <!-- <div
-      v-for="(err, index) in errorState"
-      :key="index"
-      @click="removeItem(index)"
-      class="bg-red-700 p-4 rounded-lg shadow-xl text-white my-4"
-    >
-      <div class="font-semibold text-red-200">{{ getDate }}</div>
-      <div>{{ err.errorMessage }}</div>
-      <div v-if="err.err">
-        <div>{{ err.err }}</div>
-        <div v-if="err.err.response">{{ err.err.response.data }}</div>
-      </div>
-      <div></div>
-    </div> -->
   </div>
 </template>
