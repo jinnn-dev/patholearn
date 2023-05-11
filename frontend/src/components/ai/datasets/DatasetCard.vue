227039b4cedaba03c01fd2cf68913bad60d4ddc6
James Neumann <jannesneemann@gmail.com>
2023-05-11 14:11:53 +0200

    Updated dataset card stats visualisation

--- frontend/src/components/ai/datasets/DatasetCard.vue
+++ frontend/src/components/ai/datasets/DatasetCard.vue
@@ -12,6 +12,18 @@ const props = defineProps({
   }
 });
 const { result } = useService(AiService.getSpecificDataset, true, props.dataset.id);
+
+function formatBytes(bytes: number, decimals = 2) {
+  if (!+bytes) return '0 Bytes';
+
+  const k = 1024;
+  const dm = decimals < 0 ? 0 : decimals;
+  const sizes = ['Bytes', 'KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB'];
+
+  const i = Math.floor(Math.log(bytes) / Math.log(k));
+
+  return `${parseFloat((bytes / Math.pow(k, i)).toFixed(dm))} ${sizes[i]}`;
+}
 </script>
 <template>
   <div class="bg-gray-700 p-2 rounded-lg">
@@ -19,12 +31,24 @@ const { result } = useService(AiService.getSpecificDataset, true, props.dataset.
       <div class="text-xl">{{ dataset.basename }}</div>
       <router-link :to="`/ai/datasets/${result?.id}`"><icon name="arrow-right"></icon></router-link>
     </div>
-    <div class="text-sm text-gray-200">{{ new Date(dataset.created).toLocaleDateString() }}</div>
-    <div>{{ dataset.dataset_stats.file_count }} Dateien</div>
-    <div>{{ dataset.dataset_stats.total_size / 1048576 }}</div>
-    <pre class="text-xs">{{ JSON.stringify(dataset, null, 2) }}</pre>
+    <div class="text-sm text-gray-200 mb-2">{{ new Date(dataset.created).toLocaleDateString() }}</div>
+    <div class="flex flex-col gap-2">
+      <div class="flex items-center gap-2 py-1rounded-md text-sm font-mono">
+        <icon name="files" stroke-width="0"></icon>
+        <div>
+          {{ dataset.dataset_stats.file_count }}
+        </div>
+      </div>
+      <div class="flex items-center gap-2 py-1rounded-md text-sm font-mono">
+        <icon name="hard-drive" stroke-width="0"></icon>
+        <div>
+          {{ formatBytes(dataset.dataset_stats.total_size) }}
+        </div>
+      </div>
+    </div>
+    <!-- <pre class="text-xs">{{ JSON.stringify(dataset, null, 2) }}</pre>
 
     <div>Detailed Task</div>
-    <pre class="text-xs">{{ JSON.stringify(result, null, 2) }}</pre>
+    <pre class="text-xs">{{ JSON.stringify(result, null, 2) }}</pre> -->
   </div>
 </template>
