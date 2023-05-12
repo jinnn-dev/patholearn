a56376c7cdeb1138b52a246e8e550baaa1d4c859
James Neumann <jannesneemann@gmail.com>
2023-05-12 16:07:12 +0200

    Better responsiveness

--- frontend/src/components/ai/ServerStatus.vue
+++ frontend/src/components/ai/ServerStatus.vue
@@ -11,18 +11,20 @@ const { result: slideResult, loading: slideLoading } = useService(SlideService.p
 const { result: authResult, loading: authLoading } = useService(AuthService.ping, true);
 </script>
 <template>
-  <div
-    class="flex flex-col w-full justify-center items-center rounded-lg border-[1px] bg-gray-700 border-gray-500 divide-y-[1px] divide-gray-500"
-  >
-    <status-item name="AI API" :status="aiResult === 'Ok' ? true : false" :loading="aiLoading"></status-item>
-    <status-item
-      name="AI Worker API"
-      :status="clearmlResult === 'Ok' ? true : false"
-      :loading="aiLoading"
-    ></status-item>
-    <status-item name="Websocket Server" :status="wsIsConnected" :loading="websocketLoading"> </status-item>
-    <status-item name="WSI API" :status="slideResult?.Hello === 'World' ? true : false" :loading="slideLoading">
-    </status-item>
-    <status-item name="Auth API" :status="authResult" :loading="authLoading"> </status-item>
+  <div class="flex justify-center items-center w-full">
+    <div
+      class="flex flex-col w-full xl:w-[800px] justify-center items-center rounded-lg border-[1px] bg-gray-700 border-gray-500 divide-y-[1px] divide-gray-500"
+    >
+      <status-item name="AI API" :status="aiResult === 'Ok' ? true : false" :loading="aiLoading"></status-item>
+      <status-item
+        name="AI Worker API"
+        :status="clearmlResult === 'Ok' ? true : false"
+        :loading="aiLoading"
+      ></status-item>
+      <status-item name="Websocket Server" :status="wsIsConnected" :loading="websocketLoading"> </status-item>
+      <status-item name="WSI API" :status="slideResult?.Hello === 'World' ? true : false" :loading="slideLoading">
+      </status-item>
+      <status-item name="Auth API" :status="authResult" :loading="authLoading"> </status-item>
+    </div>
   </div>
 </template>
