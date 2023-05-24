86617a67f850e04b7fd37f6cec486ee517b9f8cc
James Neumann <jannesneemann@gmail.com>
2023-05-24 12:02:14 +0200

    Added back button

--- frontend/src/views/ai/tasks/Task.vue
+++ frontend/src/views/ai/tasks/Task.vue
@@ -5,6 +5,8 @@ import { AiService } from '../../../services/ai.service';
 import NodeEditor from '../../../components/ai/builder/editor/NodeEditor.vue';
 import { usePresenceChannel } from '../../../composables/ws/usePresenceChannel';
 import { onMounted } from 'vue';
+import Icon from '../../../components/general/Icon.vue';
+
 const route = useRoute();
 
 const { loading, result: task, run } = useService(AiService.getTask);
@@ -17,6 +19,14 @@ onMounted(async () => {
 });
 </script>
 <template>
+  <router-link
+    v-if="task"
+    :to="`/ai/projects/${task.project_id}`"
+    class="fixed top-4 left-4 z-10 flex justify-center items-center py-1 px-2 cursor-pointer rounded-md gap-2 bg-gray-800 h hover:ring-1 hover:ring-gray-500 hover:bg-gray-600"
+  >
+    <icon name="arrow-left" size="18"></icon>
+    <div>Projekt</div>
+  </router-link>
   <div class="bg-gray-900 w-full h-full relative">
     <div class="fixed left-1/2 -translate-x-1/2 top-4 flex justify-center items-center z-20">
       <div class="flex gap-4 bg-gray-800/80 backdrop-blur-lg px-8 py-1 rounded-full">
