3bb5af154c5aabc28d60b1be58d2fa6e4fabf2d1
James Neumann <jannesneemann@gmail.com>
2023-05-31 09:11:53 +0200

    Refactor editor loading indicator

--- frontend/src/components/ai/builder/editor/NodeEditor.vue
+++ frontend/src/components/ai/builder/editor/NodeEditor.vue
@@ -150,48 +150,7 @@ const itemClicked = async (event: EventName) => {
         </div>
       </div>
     </div>
-    <transition name="fade">
-      <div
-        v-if="!builderState.builderLoaded && !builderState.initialGraphLoaded"
-        class="absolute select-none flex flex-col gap-4 justify-center items-center w-full h-full bg-gray-900/80 backdrop-blur-sm z-20 top-0"
-      >
-        <!-- <img alt="Viewer is loading" class="w-1/5 h-1/4" src="/blocks_loading.svg" /> -->
-        <div class="text-2xl font-bold">Editor wird geladen</div>
-        <div class="h-1 overflow-hidden w-96 mt-4">
-          <div class="loading-bar relative w-full h-1 bg-green-500"></div>
-        </div>
-      </div>
-    </transition>
+
     <div class="rete w-full h-full bg-gray-900" ref="rete"></div>
   </div>
 </template>
-
-<style scoped>
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 0.3s;
-}
-.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
-  opacity: 0;
-}
-.loading-bar {
-  animation-name: loader-animation;
-  animation-duration: 2.5s;
-  animation-iteration-count: infinite;
-  animation-timing-function: cubic-bezier(0.65, 0, 0.35, 1);
-}
-@keyframes loader-animation {
-  0% {
-    left: -100%;
-  }
-  49% {
-    left: 100%;
-  }
-  50% {
-    left: 100%;
-  }
-  100% {
-    left: -100%;
-  }
-}
-</style>
--- frontend/src/views/ai/tasks/Task.vue
+++ frontend/src/views/ai/tasks/Task.vue
@@ -66,6 +66,18 @@ watch(
         <div class="hover:ring-1 cursor-pointer ring-gray-500 hover:bg-gray-700 px-4 py-1 rounded-lg">Konsole</div>
       </div>
     </div>
+    <transition name="fade">
+      <div
+        v-if="!builderState.builderLoaded && !builderState.initialGraphLoaded"
+        class="absolute select-none flex flex-col gap-4 justify-center items-center w-full h-full bg-gray-900/80 backdrop-blur-sm z-20 top-0"
+      >
+        <!-- <img alt="Viewer is loading" class="w-1/5 h-1/4" src="/blocks_loading.svg" /> -->
+        <div class="text-2xl font-bold">Editor wird geladen</div>
+        <div class="h-1 overflow-hidden w-96 mt-4">
+          <div class="loading-bar relative w-full h-1 bg-green-500"></div>
+        </div>
+      </div>
+    </transition>
     <node-editor
       v-if="task && builderState.members.length !== 0"
       :task-id="task.id"
@@ -78,3 +90,33 @@ watch(
     <user-list-indicator :connected="isConnected" :members="members" :me="me"></user-list-indicator>
   </div>
 </template>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
+.loading-bar {
+  animation-name: loader-animation;
+  animation-duration: 2.5s;
+  animation-iteration-count: infinite;
+  animation-timing-function: cubic-bezier(0.65, 0, 0.35, 1);
+}
+@keyframes loader-animation {
+  0% {
+    left: -100%;
+  }
+  49% {
+    left: 100%;
+  }
+  50% {
+    left: 100%;
+  }
+  100% {
+    left: -100%;
+  }
+}
+</style>
