ede4707a1ef5fc8de89dacbdea219378f88cefc2
James Neumann <jannesneemann@gmail.com>
2023-06-12 22:45:41 +0200

    Update Notification style

--- frontend/src/components/general/NotificationBar.vue
+++ frontend/src/components/general/NotificationBar.vue
@@ -12,29 +12,26 @@ const removeItem = (id: string) => {
 };
 </script>
 <template>
-  <div class="fixed z-[999] top-2 right-6 transform w-80">
+  <div class="fixed z-[999] top-2 right-6 transform w-96 flex flex-col gap-2">
     <TransitionGroup tag="ul" name="notification" class="relative">
-      <notification
-        v-for="notification in notifications"
-        :key="notification.id"
-        :notification="notification"
-        @expired="removeItem($event)"
-      >
-      </notification>
+      <div :key="notification.id" v-for="notification in notifications" class="py-1.5">
+        <notification :notification="notification" @expired="removeItem($event)" />
+      </div>
     </TransitionGroup>
   </div>
 </template>
 <style>
-.notification-move,
+.notification-move {
+  transition: all 0.4s cubic-bezier(0.33, 1, 0.68, 1);
+}
 .notification-enter-active,
 .notification-leave-active {
-  transition: all 0.4s cubic-bezier(0.36, 0, 0.66, -0.56);
+  transition: all 0.4s cubic-bezier(0.65, 0, 0.35, 1);
 }
 
 .notification-enter-from,
 .notification-leave-to {
-  opacity: 0;
-  transform: translateX(100px);
+  transform: translateX(110%);
 }
 
 .notification-leave-active {
