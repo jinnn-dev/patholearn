f3e92ac52972af97e62a9084345387119d5c03a1
JamesNeumann <jannesneemann@gmail.com>
2023-06-15 18:26:37 +0000

    Update worker deployment

--- ai-worker/app/ws/client.py
+++ ai-worker/app/ws/client.py
@@ -1,13 +1,16 @@
 import os
 from pusher import Pusher
 
+
 ws_client = Pusher(
     app_id=os.environ.get("WEBSOCKET_APP_ID"),
     key=os.environ.get("WEBSOCKET_APP_KEY"),
     secret=os.environ.get("WEBSOCKET_APP_SECRET"),
     host=os.environ.get("WEBSOCKET_HOST"),
-    port=int(os.environ.get("WEBSOCKET_PORT"))
-    if "WEBSOCKET_PORT" in os.environ
-    else None,
+    port=(
+        None
+        if "WEBSOCKET_PORT" not in os.environ or os.environ.get("WEBSOCKET_PORT") == ""
+        else int(os.environ.get("WEBSOCKET_PORT"))
+    ),
     ssl=False if os.environ.get("WEBSOCKET_SSL") == "False" else True,
 )
