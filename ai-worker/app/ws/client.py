730f884c50b7ba38175d7b6c2727ba4eb672d770
JamesNeumann <jannesneemann@gmail.com>
2023-06-15 08:07:20 +0000

    Update host

--- ai-worker/app/ws/client.py
+++ ai-worker/app/ws/client.py
@@ -5,7 +5,7 @@ ws_client = Pusher(
     app_id=os.environ.get("WEBSOCKET_APP_ID"),
     key=os.environ.get("WEBSOCKET_APP_KEY"),
     secret=os.environ.get("WEBSOCKET_APP_SECRET"),
-    host="ws",
+    host=os.environ.get("WEBSOCKET_HOST"),
     port=int(os.environ.get("WEBSOCKET_PORT"))
     if "WEBSOCKET_PORT" in os.environ
     else None,
