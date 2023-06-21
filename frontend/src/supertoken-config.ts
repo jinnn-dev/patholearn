a59f9cfe04d92f95ed5bf60bb17f0a48dd4544b5
James Neumann <jannesneemann@gmail.com>
2023-06-21 16:47:13 +0200

    Added debug logs

--- frontend/src/supertoken-config.ts
+++ frontend/src/supertoken-config.ts
@@ -1,6 +1,7 @@
 import Session from 'supertokens-web-js/recipe/session';
 import EmailPassword from 'supertokens-web-js/recipe/emailpassword';
 export const SuperTokensWebJSConfig = {
+  enableDebugLogs: true,
   appInfo: {
     appName: 'Patholearn AUth',
     apiDomain: import.meta.env.VITE_AUTH_API_URL,
