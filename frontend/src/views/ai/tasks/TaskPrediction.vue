68022de5a51d3f7e5b6a0aa0c6803fda3438a585
JamesNeumann <jannesneemann@gmail.com>
2023-07-15 17:47:02 +0200

    Fix title and padding

--- frontend/src/views/ai/tasks/TaskPrediction.vue
+++ frontend/src/views/ai/tasks/TaskPrediction.vue
@@ -35,7 +35,7 @@ const getRoundedPercentage = (num: number) => {
 };
 </script>
 <template>
-  <div class="text-center text-4xl pb-2">Konsole</div>
+  <div class="text-center text-4xl pb-2">Prediction</div>
   <div class="flex flex-col items-center justify-center h-full">
     <div v-if="!builderState.task">Loading...</div>
 
@@ -45,14 +45,14 @@ const getRoundedPercentage = (num: number) => {
     >
       Training noch nicht abgeschlossen
     </div>
-    <div v-else class="flex gap-12 w-full xl:w-2/3 items-center h-full px-4 pb-32">
+    <div v-else class="flex gap-12 w-full xl:w-2/3 items-center h-full px-4 pb-24">
       <image-dropzone @images-dropped="makePrediction"></image-dropzone>
       <div v-if="!predictionFile" class="flex items-center">
         <icon name="arrow-left" class="text-gray-300" size="56"></icon>
         <div class="text-3xl text-gray-300 font-semibold w-full">Add an image to ge a prediction</div>
       </div>
       <div v-else class="transition-all w-full max-h-full flex flex-col justify-center">
-        <div class="max-h-full overflow-auto" v-if="result && result.dataset.class_map">
+        <div class="max-h-full overflow-auto pr-2" v-if="result && result.dataset.class_map">
           <div class="flex flex-col w-full justify-center gap-1 my-2" v-for="label_index in result?.dataset.classes">
             <div class="font-mono text-lg">{{ result.dataset.class_map[label_index] }}</div>
             <div class="flex gap-2 w-full">
