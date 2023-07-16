766f21b14156b1299eb48debbc947f6c28ef80a9
JamesNeumann <jannesneemann@gmail.com>
2023-07-16 14:03:47 +0200

    Refactor and task for checking serving status

--- ai-worker/app/core/serve.py
+++ ai-worker/app/core/serve.py
@@ -1,5 +1,10 @@
 from clearml import Task, Dataset, Model
 from app.utils.process import run_command
+import requests
+import base64
+import numpy as np
+from PIL import Image
+from io import BytesIO
 
 
 def serve_model(clearml_task: Task, dataset_id: str):
@@ -51,3 +56,38 @@ def serve_model(clearml_task: Task, dataset_id: str):
 def remove_model(clearml_task_id: str):
     command = ["clearml-serving", "model", "remove", "--endpoint", clearml_task_id]
     return run_command(command)
+
+
+def get_prediction(clearml_task_id: str, image_data: bytes, metadata: dict):
+    encoded_image = base64.b64encode(image_data)
+    result = requests.post(
+        f"http://10.168.2.83:8080/serve/{clearml_task_id}",
+        json={"image": encoded_image.decode(), "metadata": metadata},
+        headers={"Content-Type": "application/json"},
+    )
+    data = result.json()
+    if "detail" in data.keys():
+        raise Exception("Did not work")
+    return data["propabilities"][0]
+
+
+def check_if_model_available(clearml_task_id: str, dataset_id: str):
+    dataset = Dataset.get(dataset_id=dataset_id)
+    metadata = dataset.get_metadata()
+    channels = 1 if metadata["is_grayscale"] else 3
+    if channels == 1:
+        imarray = np.random.rand(100, 100) * 255
+    else:
+        imarray = np.random.rand(100, 100, channels) * 255
+    im = Image.fromarray(imarray.astype("uint8")).convert("RGBA")
+    im_bytes = BytesIO()
+    im.save(im_bytes, "PNG")
+    try:
+        get_prediction(
+            clearml_task_id=clearml_task_id,
+            image_data=im_bytes.getvalue(),
+            metadata=metadata,
+        )
+        return True
+    except Exception:
+        return False
--- ai-worker/app/scheduler/modify.py
+++ ai-worker/app/scheduler/modify.py
@@ -1,8 +1,54 @@
+import json
+
 from app.scheduler.schedulers import PeriodicTask
+from app.scheduler.session import SessionManager
+from app.scheduler.models import IntervalSchedule, PeriodicTask
+
+beat_dburi = "sqlite:///app/scheduler/schedule.db"
+
+session_manager = SessionManager()
+session = session_manager.session_factory(beat_dburi)
+session.close()
 
 
-def remove_periodic_task(clearml_task_id: str, session):
+def remove_periodic_task(clearml_task_id: str, session=None):
+    if not session:
+        _, session_maker = session_manager.create_session(beat_dburi)
+        session = session_maker()
     periodic_task = session.query(PeriodicTask).filter_by(name=clearml_task_id).first()
     if periodic_task is not None:
         session.delete(periodic_task)
         session.commit()
+
+
+def create_periodic_task(
+    peridodic_task_name: str,
+    task_name: str,
+    data: dict,
+    interval: int = 5,
+    session=None,
+):
+    if not session:
+        _, session_maker = session_manager.create_session(beat_dburi)
+        session = session_maker()
+    schedule = (
+        session.query(IntervalSchedule)
+        .filter_by(every=interval, period=IntervalSchedule.SECONDS)
+        .first()
+    )
+    if not schedule:
+        schedule = IntervalSchedule(every=5, period=IntervalSchedule.SECONDS)
+        session.add(schedule)
+        session.commit()
+
+    task = PeriodicTask(
+        interval=schedule,
+        name=peridodic_task_name,
+        task=task_name,
+        kwargs=json.dumps(data),
+        queue="ai",
+    )
+    session.add(task)
+    session.commit()
+
+    return schedule
