ef666f0645dbf1344f5d2ef0f4ab628f80eb3281
JamesNeumann <jannesneemann@gmail.com>
2023-07-16 14:04:15 +0200

    Check if serving is available

--- frontend/src/views/ai/tasks/TaskMetrics.vue
+++ frontend/src/views/ai/tasks/TaskMetrics.vue
@@ -3,11 +3,11 @@ import { builderState } from '../../../core/ai/builder/state';
 import Metrics from '../../../components/ai/tasks/Metrics.vue';
 </script>
 <template>
-  <div class="text-center text-4xl pb-2">Metriken</div>
+  <div class="text-center text-4xl pb-2">Metrics</div>
   <div class="flex flex-col items-center justify-center h-full">
     <div v-if="!builderState.task">Loading...</div>
-    <div v-else-if="!builderState.selectedVersion?.clearml_id" class="text-4xl text-gray-300">
-      Training läuft noch nicht
+    <div v-else-if="!builderState.selectedVersion?.clearml_id" class="text-3xl text-gray-300">
+      Training is not running yet
     </div>
     <div v-else class="w-full h-full px-4 pb-32 overflow-auto">
       <Metrics :clear-ml-task-id="builderState.selectedVersion!.clearml_id!"></Metrics>
